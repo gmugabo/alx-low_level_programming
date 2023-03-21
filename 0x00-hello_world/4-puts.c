8a4e2f8a0b62a4fc4b8558e225b6f1b2a22b0911
gmugabo <mugabogad88@gmail.com>
2023-03-20 17:54:48 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - print the string in the code
+ * Return: always 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+}
