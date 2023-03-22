9f9318ca95fcb15740765eec2a31ddd1fdf61013
gmugabo <mugabogad88@gmail.com>
2023-03-21 17:53:29 -0700

    0-putchar.c

---
+++ 0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description:'Write a program that prints _putchar, followed by a new line'
+ *
+ *
+ * Return: always zero
+ */
+
+int main(void)
+{
+	_putchar('\n');
+	return (0);
+}
