cec06a6fbee61e9e2e0709c3aefd298028439205
gmugabo <mugabogad88@gmail.com>
2023-03-30 18:00:22 -0700

    functions that changes all lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -2,24 +2,24 @@
 
 /**
  * string_toupper - changes all lowercases to uppercases
- * @str: pointer to string
+ * @st: pointer to string
  *
  * Return: pointer to uppercase string
  */
 
-char *string_toupper(char *str)
+char *string_toupper(char *st)
 {
 	int i;
 
 	i = 0;
 
-	while (str[i] != '\0')
+	while (st[i] != '\0')
 	{
-		if (str[i] >= 97 && str[i] <= 122)
+		if (st[i] >= 97 && st[i] <= 122)
 		{
-			str[i] = str[i] - 32;
+			st[i] = st[i] - 32;
 		}
 		i++;
 	}
-	return (0);
+	return (st);
 }
