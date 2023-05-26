d21c448b4005dfc1a09af5b380b3846b50db8f14
gmugabo <mugabogad88@gmail.com>
2023-05-26 13:48:53 -0700

    added description for return value

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -4,6 +4,8 @@
  * set_bit - sets a bit at an index to 1
  * @n: pointer to number change
  * @index: index of the bit set to 1
+ *
+ * Return: 1 for success, -1 for failure
  */
 
 int set_bit(unsigned long int *n, unsigned int index)
